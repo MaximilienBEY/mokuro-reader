f0355047f10c954af92b2a15ebf8893bfb7cec87
Maximilien Bey <bey.maximilien@gmail.com>
2024-09-13 10:36:18 +0200

    pad time

--- src/lib/util/time.ts
+++ src/lib/util/time.ts
@@ -1,8 +1,10 @@
 export const parseTime = (time: number) => {
   // seconds
   time = time * 60;
-  const hours = Math.floor(time / 3600);
-  const minutes = Math.floor((time % 3600) / 60);
-  const seconds = Math.floor(time % 60);
-  return `${hours}h ${minutes}m ${seconds}s`;
+  const hours = Math.floor(time / 3600).toString().padStart(2, '0');
+  const minutes = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
+  const seconds = Math.floor(time % 60).toString().padStart(2, '0');
+
+  if (hours === '00') return `${minutes}:${seconds}`;
+  else return `${hours}:${minutes}:${seconds}`;
 }
